b56726d8f4a80fe6b47a4c0aa98e09e3613c6a2a
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-05-29 10:55:44 +0700

    Add template for writing problems

---
+++ leetcode/template/README.md
@@ -0,0 +1,13 @@
+### tl;dr
+
+[Link to the original problem]()
+
+### Idea
+
+### Code
+
+```python
+# complexity with n = len(A):
+# time: O(n)
+# space: O(n)
+```
---
+++ leetcode/template/test.py
@@ -0,0 +1,8 @@
+import unittest
+from solution import Solution
+
+class Test(unittest.TestCase):
+    def test_general_cases(self):
+
+    def test_edge_cases(self):
+        # tbd more when reading canvas
