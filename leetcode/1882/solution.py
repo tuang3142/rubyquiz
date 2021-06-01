8a7b8f50b5841b8bdad95b789807cdcb49443a21
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-01 09:24:23 +0700

    Documented the solution

--- leetcode/1882/solution.py
+++ leetcode/1882/solution.py
@@ -3,13 +3,13 @@ import heapq
 class Solution:
     def assignTasks(self, servers, tasks):
         busy = []
-        free = [[0, w, i] for servers] # server_time, weight, index
+        free = [[0, w, i] for servers]
         heapq.heapify(free)
         ret = []
         for now, process_time in enumerate(tasks):
-            while busy and (busy[0][0] <= now or not free): # check for servers that finish their tasks
-                st, w, i = heapq.heappop(busy)              # or pop anyway if there are no free servers
-                if st <= now: st = 0 # equalize all free servers' times to prioritize by their weights later
+            while busy and (busy[0][0] <= now or not free):
+                st, w, i = heapq.heappop(busy)
+                if st <= now: st = 0
                 heapq.heappush(free, [st, w, i])
             st, w, i = heapq.heappop(free)
             ret.append(i)
