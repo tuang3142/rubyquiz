c6cdd793c63c7a48cd4b127bcedb00fa2e93fcc1
Tuang Nguyen <tuan.nguyenviet271@gmail.com>
2021-06-05 13:10:01 +0700

    1778 refactor solution

--- leetcode/1778/solution.py
+++ leetcode/1778/solution.py
@@ -2,39 +2,37 @@ DIR = { "U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1) }
 BACK = { "U": "D", "L": "R", "D": "U", "R": "L" }
 N = 500
 
-# maybe apply struct like python
 class Solution(object):
     def __init__(self):
-        self.found = False
-        self.grid = [[-1] * (N * 2) for _ in range(N * 2)] # -1: unexplored, 0: empty, 1: blocked, 2: target
+        self.grid = [[-1] * (N * 2) for _ in range(N * 2)]
 
-    def findShortestPath(self, gm):
-        self.dfs(gm, N, N)
-        if not self.found: return -1
+    def findShortestPath(self, robot):
+        if not self.dfs(robot, N, N): return -1
         return self.bfs()
 
-    def dfs(self, gm, i, j):
-        if self.grid[i][j] != -1: return # visited
-        if gm.isTarget():
-            self.grid[i][j] = 2
-            self.found = True
-        else:
-            self.grid[i][j] = 0
+    def dfs(self, robot, i, j):
+        if self.grid[i][j] != -1: return
+        self.grid[i][j] = 2 if robot.isTarget() else 0
+        found = True if robot.isTarget() else False
         for d in DIR.keys():
-            x, y = i + DIR[d][0], j + DIR[d][1]
-            if not gm.canMove(d):
+            x, y = self.move_from(i, j, d)
+            if not robot.canMove(d):
                 self.grid[x][y] = 1
                 continue
-            gm.move(d)
-            self.dfs(gm, x, y)
-            gm.move(BACK[d])
+            robot.move(d)
+            if self.dfs(robot, x, y): found = True
+            robot.move(BACK[d])
+        return found
 
     def bfs(self):
         qu = [(N, N, 0)]
         for i, j, dis in qu:
             if self.grid[i][j] == 2: return dis
-            if self.grid[i][j] != 0: continue
-            self.grid[i][j] = 1 # block it to not go back
+            if self.grid[i][j] == 1: continue
+            self.grid[i][j] = 1
             for d in DIR.keys():
-                x, y = i + DIR[d][0], j + DIR[d][1]
+                x, y = self.move_from(i, j, d)
                 qu.append((x, y, dis + 1))
+
+    def move_from(self, i, j, d):
+        return i + DIR[d][0], j + DIR[d][1]
